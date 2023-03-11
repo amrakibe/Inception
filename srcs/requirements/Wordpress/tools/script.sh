8581f40c0e2cda4ce1b36a553bed50ea0cf44243
amine rakibe <aminerakibe7@gmail.com>
2023-03-11 17:51:47 +0100

    <>

--- srcs/requirements/Wordpress/tools/script.sh
+++ srcs/requirements/Wordpress/tools/script.sh
@@ -5,6 +5,10 @@ cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 rm -rf latest.tar.gz
-mv ./wordpress/* .
+cp -r ./wordpress/* .
+
+
+#sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+#mkdir /run/php
 
 /usr/sbin/php-fpm7.3 -F
