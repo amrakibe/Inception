64b2764740455abbd1816c429f2e6b701fb4b118
amine rakibe <aminerakibe7@gmail.com>
2023-03-11 14:38:56 +0100

    upload

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,10 @@
+
+service mysql start
+echo "CREATE DATABASE wp;" > file.sql
+echo "CREATE USER amrakibe@'%' IDENTIFIED BY '1234';" >> file.sql
+echo "grant all privileges on wp.* to amrakibe;" >> file.sql
+echo "FLUSH PRIVILEGES;">> file.sql
+
+mysql < file.sql
+
+mysqld_safe
