1b0c67ffd8ed210a6183a2fda60cae62da25c967
amine rakibe <88275493+amrakibe@users.noreply.github.com>
2023-04-14 18:31:34 +0000

    fixed error

--- Makefile
+++ Makefile
@@ -4,7 +4,11 @@ run:
 stop:
 	@docker-compose -f ./srcs/docker-compose.yml stop
 	@echo "docker-compose stop successfull"
-rm: stop
+rm:
+	@docker volume rm srcs_db
+	@docker volume rm srcs_wp
+	@echo "docker-compose remove volumes successfull"
+rm_v:
 	@docker rm  $$(docker ps -a -q) -f
 	@docker rmi  $$(docker images -a -q) -f
-	@echo "docker-compose remove images and containers successfull"
+	@docker network prune -f
--- srcs/requirements/Wordpress/tools/script.sh
+++ srcs/requirements/Wordpress/tools/script.sh
@@ -7,7 +7,28 @@ tar -xzvf latest.tar.gz
 rm -rf latest.tar.gz
 cp -r ./wordpress/* .
 
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /var/www/html
+wp core download --allow-root
+
+
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+mv /wp-config.php /var/www/html/wp-config.php
+
+
+sed -i -r "s/database_name_here/${MYSQL_DATABASE}/g" wp-config.php
+sed -i -r "s/username_here/${MYSQL_USER}/g" wp-config.php
+sed -i -r "s/password_here/${MYSQL_PASSWORD}/g" wp-config.php
+
+# configure wordpress
+wp core install --url=${DOMAIN_NAME}/ --title=${WP_TITLE} --admin_user=${ADMIN_WP_USER} --admin_password=${ADMIN_WP_PASSWORD} --admin_email=${ADMIN_WP_EMAIL} --skip-email --allow-root
+wp user create ${WP_USR} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
+wp plugin update --all --allow-root
+
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-mkdir /run/php
+mkdir -p /run/php
 
 /usr/sbin/php-fpm7.3 -F
